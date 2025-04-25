d2c46205d34a9cf69811bb50cf3ed69545686890
Aleksandras Jarmalavičius <59929032+Aleksandras95@users.noreply.github.com>
2025-04-26 00:03:28 +0300

    initial commit

---
+++ components/FormComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <form>
+        <div class="flex items-center justify-between">
+            <input class="border border-solid border-gray-500 rounded-xl px-6 py-3 outline-none w-3/5" type="text" v-model="task">
+            <button class="bg-green-600 px-6 py-3 rounded-xl text-white uppercase text-base font-medium hover:bg-green-800">Submit</button>
+        </div>
+    </form>
+</template>
+
+<script setup>
+const task = ref('')
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="border border-solid border-gray-500 rounded-3xl p-5 max-w-[600px] mx-auto">
+        <FormComponent />
+    </div>
+</template>
